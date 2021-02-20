2236aa7e92de43b5c13c1163bd166ddbe4b4414d
SashimiDude <51582041+SashimiDude@users.noreply.github.com>
2021-02-20 12:37:15 +1100

    Adjusted constant values to make them much lower
    Needs further testing

--- TestBench1/TestBench/src/main/java/frc/robot/motorchecking/motorCheckConstants.java
+++ TestBench1/TestBench/src/main/java/frc/robot/motorchecking/motorCheckConstants.java
@@ -3,14 +3,14 @@ package frc.robot.motorchecking;
 public class motorCheckConstants {
 
     //Global
-    public static final int kControllerBusVoltageShutdown = 10;
-    public static final int kControllerBusVoltageWarning = 12;
+    public static final int kControllerBusVoltageShutdown = 2;
+    public static final int kControllerBusVoltageWarning = 4;
     public static final int kMotorTemperatureShutdown = 80;
     public static final int kMotorTemperatureWarning = 55;
-    public static final int kPDPOutputCurrentShutdown = 1;
+    public static final  double kPDPOutputCurrentShutdown = 0.8;
     public static final int kPDPOutputCurrentWarning = 2;
-    public static final int kMotorOutputCurrentShutdown = 20;
-    public static final int kMotorOutputCurrentWarning = 25;
+    public static final double kMotorOutputCurrentShutdown = 0.5;
+    public static final int kMotorOutputCurrentWarning = 2;
     public static final int kEncoderVelocityShutdown = 5;
     public static final int kEncoderVelocityWarning = 15;
     public static final double kMinimumSpeedForCheck = 0.4;
